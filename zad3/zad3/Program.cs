c9a2374d44b894574e5891c1aefa752d5ebd331d
czaja4 <czajkowskia12345@gmail.com>
2025-04-11 00:43:43 +0200

    Add files via upload

---
+++ zad3/zad3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string[] imiona = { "Ania", "Kasia", "Basia", "Zosia" };
+        string[] nazwiska = { "Kowalska", "Nowak" };
+        Random random = new Random();
+
+        string userFile = $@"C:\test\users_{DateTime.Now:yyyyMMdd_HHmm}.csv";
+
+        using (StreamWriter writer = new StreamWriter(userFile))
+        {
+            writer.WriteLine("LP;Imie;Nazwisko;Rok_Urodzenia");
+            for (int i = 1; i <= 100; i++)
+            {
+                string imie = imiona[random.Next(imiona.Length)];
+                string nazwisko = nazwiska[random.Next(nazwiska.Length)];
+                int rok = random.Next(1990, 2001);
+                writer.WriteLine($"{i};{imie};{nazwisko};{rok}");
+            }
+        }
+        Console.WriteLine($"plik: {userFile}");
+    }
+}
