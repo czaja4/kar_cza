8ac42a0a57f0920610f11f3dee4fd38628fec74c
czaja4 <czajkowskia12345@gmail.com>
2025-04-11 00:43:11 +0200

    Add files via upload

---
+++ zad2/zad2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string plik = @"C:\test\zad2.txt";
+
+        if (File.Exists(plik))
+        {
+            string content = File.ReadAllText(plik, Encoding.UTF8).Replace("praca", "job");
+            string data = DateTime.Now.ToString("yyyyMMdd");
+            string nowyplik = plik.Replace(".txt", $"_changed_{data}.txt");
+
+            File.WriteAllText(nowyplik, content, Encoding.UTF8);
+            Console.WriteLine($"Plik zapisany jako: {nowyplik}");
+        }
+        else
+        {
+            Console.WriteLine("nie znalaziono pliku");
+        }
+    }
+}
