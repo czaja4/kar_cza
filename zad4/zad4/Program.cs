e38b8635a53b908b0ce73d53163a6f8ba7792e0c
czaja4 <czajkowskia12345@gmail.com>
2025-04-11 00:44:24 +0200

    Add files via upload

---
+++ zad4/zad4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj kwotę w PLN: ");
+        if (decimal.TryParse(Console.ReadLine(), out decimal pln))
+        {
+            Console.Write("Podaj kurs wymiany PLN na USD: ");
+            if (decimal.TryParse(Console.ReadLine(), out decimal kurs) && kurs > 0)
+            {
+                Console.WriteLine($"Kwota w USD: {pln / kurs:f2}");
+            }
+            else
+            {
+                Console.WriteLine("cos zle zrobiles");
+            }
+        }
+        else
+        {
+            Console.WriteLine("zle wpisales kwote");
+        }
+    }
+}
