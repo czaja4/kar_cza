8caa4fd03359ff5719d1ae1c4aace322a81036b6
czaja4 <czajkowskia12345@gmail.com>
2025-04-11 00:42:28 +0200

    Add files via upload

---
+++ zad1/zad1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = @"c:\test\test_kar_wac.txt";
+
+        if (File.Exists(filePath))
+        {
+            string content = File.ReadAllText(filePath);
+            int count = content.Count(c => c == 'a' || c == 'A');
+            Console.WriteLine($"Liczba wystąpień litery 'a': {count}");
+        }
+        else
+        {
+            Console.WriteLine("Plik nie istnieje.");
+        }
+    }
+}
